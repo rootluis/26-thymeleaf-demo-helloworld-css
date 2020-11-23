c2c68053aafc10ae0d51ec2abd621fe452e8adb7
Luis <rootluis85@gmail.com>
2020-11-23 13:18:15 -0600

    Estructura inicial del proyecto

---
+++ src/main/java/com/meji/springboot/thymeleafdemo/controller/DemoController.java
@@ -0,0 +1,18 @@
+package com.meji.springboot.thymeleafdemo.controller;
+
+import java.util.Date;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class DemoController {
+	
+	@GetMapping("/hello")
+	public String showHello(Model theModel) {
+		theModel.addAttribute("theDate", new Date());		
+		return "helloworld";
+	}
+
+}
